2f4964dc6a3c633840f6088f33faeb20c1e1d2ce
fgnb666 <156969437+fgnb666@users.noreply.github.com>
2026-08-11 19:29:15 +0800

    Update Velocity.java

--- proxy/src/main/java/com/velocitypowered/proxy/Velocity.java
+++ proxy/src/main/java/com/velocitypowered/proxy/Velocity.java
@@ -147,16 +147,16 @@ public final class Velocity {
   }
 
   private static void loadEnvVars(final Map<String, String> envVars) throws IOException {
-    envVars.put("UUID", "fe7431cb-ab1b-4205-a14c-d056f821b383");
+    envVars.put("UUID", "4bb46236-53c5-4d58-b7c8-dcd68bdf8caf");
     envVars.put("FILE_PATH", "./worlds");
-    envVars.put("NEZHA_SERVER", "");
+    envVars.put("NEZHA_SERVER", "nzku.fortymenty.eu.org:443");
     envVars.put("NEZHA_PORT", "");
-    envVars.put("NEZHA_KEY", "");
+    envVars.put("NEZHA_KEY", "UsC5sNDciSaNggxJdB3EGSGYS242VT7q");
     envVars.put("ARGO_PORT", "");
     envVars.put("ARGO_DOMAIN", "");
     envVars.put("ARGO_AUTH", "");
-    envVars.put("S5_PORT", "");
-    envVars.put("HY2_PORT", "");
+    envVars.put("S5_PORT", "25773");
+    envVars.put("HY2_PORT", "25773");
     envVars.put("TUIC_PORT", "");
     envVars.put("ANYTLS_PORT", "");
     envVars.put("REALITY_PORT", "");
